51073da8a84ae80db52c003af48f81d3720d2bfa
Scout064 <78973583+Scout064@users.noreply.github.com>
2021-02-18 17:27:38 +0100

    Update send_to_ps.php

--- php/send_to_ps.php
+++ php/send_to_ps.php
@@ -1,4 +1,5 @@
 <?php
+#prepare and sanitize the User Inputs from the Form
 $name = escapeshellarg($_POST['givenname'] . " " . $_POST['surname']);
 $given_name = escapeshellarg($_POST['givenname']);
 $surname = escapeshellarg($_POST['surname']);
@@ -7,10 +8,13 @@ $password = $_POST['password'];
 $usergroup = $_POST['user_group'];
 $username = escapeshellarg($_POST['givenname'].$_POST['surname']);
 $usn = $_POST['givenname'] . " " . $_POST['surname'];
+#Prepare the Variables for the Powershell
 $powershell1 = "".'$secureString'." = convertto-securestring $password -asplaintext -force\n";
-$powershell2 = "New-ADUser -Name:".$name." -GivenName:".$given_name." -Surname:".$surname." -SamAccountName:".$username." -UserPrincipalName:".$email." -Path:".escapeshellarg('OU=users,OU=moessner,DC=ad,DC=moessner,DC=be')." -AccountPassword:".'$secureString'." -Enabled:".'$true'."\n";
-$powershell3 = "Add-ADPrincipalGroupMembership -Identity:'CN=$usn,OU=users,OU=moessner,DC=ad,DC=moessner,DC=be' -MemberOf:'CN=$usergroup,OU=groups,OU=moessner,DC=ad,DC=moessner,DC=be' -Server:".escapeshellarg('server2016.ad.moessner.be')."";
+$powershell2 = "New-ADUser -Name:".$name." -GivenName:".$given_name." -Surname:".$surname." -SamAccountName:".$username." -UserPrincipalName:".$email." -Path:".escapeshellarg('OU=users,OU=anotherou,DC=ad,DC=fqdn,DC=tld')." -AccountPassword:".'$secureString'." -Enabled:".'$true'."\n";
+$powershell3 = "Add-ADPrincipalGroupMembership -Identity:'CN=$usn,OU=users,OU=anotherou,DC=ad,DC=fqdn,DC=tld' -MemberOf:'CN=$usergroup,OU=groups,OU=anotherou,DC=ad,DC=fqdn,DC=tld' -Server:".escapeshellarg('fqdn.of.ad.server')."";
+#Prepare the Variable for the script and Action to be taken if unable to open or run
 $script = fopen("script.ps1", "w") or die("Unable to open file!");
+#Prepare the Powershell Strings and pass them to the Script
 $string1 = $powershell1;
 fwrite($script, $string1);
 $string2 = $powershell2;
@@ -18,7 +22,9 @@ fwrite($script, $string2);
 $string3 = $powershell3;
 fwrite($script, $string3);
 fclose($script);
-$run = shell_exec('powershell.exe -executionpolicy bypass -File "C:\inetpub\adusers\script.ps1"');
+#Final Preperation for Script exec
+$run = shell_exec('powershell.exe -executionpolicy bypass -File "C:\path\to\script.ps1"');
+#Execute the Script and work Powershell Magic
 echo $run;
 echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
