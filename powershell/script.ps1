afc25afc40bf351ea683fe51d0bb50683c4358e6
Scout064 <78973583+Scout064@users.noreply.github.com>
2021-03-01 19:09:37 +0100

    Delete script.ps1

--- powershell/script.ps1
+++
@@ -1,6 +0,0 @@
-#Setup Variable for the Password
-$secureString = convertto-securestring YourPassword -asplaintext -force
-#Actual Powershell Command to add  the AD-User
-New-ADUser -Name:"Max Mustermann" -GivenName:"Max" -Surname:"Mustermann" -SamAccountName:"MaxMustermann" -UserPrincipalName:"max@mail.com" -Path:"OU=users,OU=anotherou,DC=ad,DC=fqdn,DC=tld" -AccountPassword:$secureString -Enabled:$true
-#Powershell Command to add User to a Group
-Add-ADPrincipalGroupMembership -Identity:'CN=Max Mustermann,OU=users,OU=anotherou,DC=ad,DC=fqdn,DC=tld' -MemberOf:'CN=group_cn,OU=groups,OU=anotherou,DC=ad,DC=fqdn,DC=tld' -Server:"fqdn.of.ad.server"
